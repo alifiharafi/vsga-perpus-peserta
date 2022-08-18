5a15eae3d034462dba71cfebeb8c191452dc620c
Riza <alifiharafi@yahoo.com>
2022-08-18 20:43:57 +0700

    fix typo index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<header>
 			<div id="header-banner" class="text-center">
 				<h1>Perpustakaan Umum</h1>
-				<h3>Jalan Kemerderkaan Indonesia No. 17 Bandung 1945</h3>
+				<h3>Jalan Kemerdekaan Indonesia No. 17 Bandung 1945</h3>
 				<p>Telepon: (022) 7203155</h3>
 			</div>
 			<div id="user-info">
